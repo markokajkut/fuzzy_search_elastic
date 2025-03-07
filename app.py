9870f55b88e22b0598fda3632ed4cbbdce58f2ce
Marko Kajkut <markokajkut1@gmail.com>
2025-03-07 10:21:05 +0100

    demo

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from fuzzy_worker import FuzzyWorker
 
 load_dotenv()
 
-my_logger = MyLogger()
+my_logger = MyLogger().get_logger()
 
 st.set_page_config(
     page_title="Fuzzy Search",
@@ -21,29 +21,95 @@ st.set_page_config(
     }
 )
 
-st.html("<div style='text-align: center; font-size: 32px; font-weight: bold;'>Search for Keywords</div>")
-es_client = Elasticsearch(os.getenv("ES_HOST"), basic_auth=(os.getenv("ES_USER"), os.getenv("ES_PASS")))
-index_name = None
+if 'es_client' not in st.session_state:
+    st.session_state['es_client'] = Elasticsearch(os.getenv("ES_HOST"), basic_auth=(os.getenv("ES_USER"), os.getenv("ES_PASS")))
+    
+# Initialize search filters if not already in session state
+if "search_filters" not in st.session_state:
+    st.session_state["search_filters"] = {}
 
-with st.sidebar:
-    uploaded_file = st.file_uploader(
-        "Choose a CSV File",
-        type=["csv"],
-        help="Load a CSV File to Search Over", 
-    )
-    
-if uploaded_file is not None:
-    fuzzy_worker = FuzzyWorker(my_logger, es_client, uploaded_file)
-    fuzzy_worker.clean_dataframe()
-    st.dataframe(fuzzy_worker.dataframe)
+# Function to reset search filters
+def reset_search_filters(fields):
+    for field in fields:
+        if field in st.session_state:
+            del st.session_state[field]  # Remove field from session state
+    st.session_state["search_filters"] = {}  # Reset dictionary
 
-search_filters = {}
-with st.sidebar:
-    for column in fuzzy_worker.dataframe.columns:
-        search_filters[column] = st.text_input(column)
-        fuzzy_worker.dataframe = fuzzy_worker.search_elasticsearch(search_filters[column], column)
-st.info(search_filters)
-    
-    
+
+def main():
+
+    st.html("<div style='text-align: center; font-size: 32px; font-weight: bold;'>Search for Keywords</div>")
+
+    fuzzy_worker = FuzzyWorker(my_logger, st.session_state['es_client'])
+
+    with st.sidebar:
         
+        index_selection = st.selectbox(
+        "Select Elasticsearch index to visualize",
+        fuzzy_worker.get_all_indexes(),
+        )
         
+        st.write("---")
+        
+        with st.form("my-form", clear_on_submit=True):
+            uploaded_file = st.file_uploader(
+                "Choose a CSV File to push to Elasticsearch",
+                type=["csv"],
+                help="Load a CSV File to Search Over", 
+            )
+            submitted = st.form_submit_button("Upload file.")
+        
+        st.write("---")
+        reset_filters = st.button("Reset Filters", type="primary")
+
+    if submitted and uploaded_file is not None:
+        resp = fuzzy_worker.read_and_push_to_es(uploaded_file)
+        st.rerun()
+        if resp:
+            st.sidebar.error(resp)
+        
+    # Get the fields from Elasticsearch
+    fields = fuzzy_worker.get_index_fields(index_name=index_selection)
+
+    # Ensure fields exist in session state before rendering widgets
+    for field in fields:
+        if field not in st.session_state["search_filters"]:
+            st.session_state["search_filters"][field] = ""
+
+
+    if len(fields) > 0:
+        # Create dynamic column layout with a max of 3 columns
+        num_columns = min(3, len(fields))  # Max 3 columns
+        columns = st.columns(num_columns)  # Create columns dynamically
+
+    # Loop through fields and distribute across columns
+    for i, field in enumerate(fields):
+        col = columns[i % num_columns]
+        with col:
+            st.session_state["search_filters"][field] = st.text_input(
+                field, 
+                value=st.session_state.get(field, ""), 
+                key=field
+            )
+
+    if reset_filters:
+        reset_search_filters(fields)
+        st.rerun()  # Force UI refresh
+
+    search_filters = {field: value for field, value in st.session_state["search_filters"].items() if value.strip()}
+
+    # Debug: Show the search filters dictionary
+    # st.write(st.session_state["search_filters"])
+
+    if not search_filters:  # If no filters applied, show full dataset
+        df = fuzzy_worker.get_data_from_es_index(index_selection)
+    else:  # If filters are applied, perform search
+        df = fuzzy_worker.multi_search_elasticsearch(index_name=index_selection, queries=search_filters, fields=list(search_filters.keys()))
+
+    if index_selection:
+        #st.write(df.dtypes)
+        st.dataframe(df)
+        
+   
+if __name__ == "__main__":
+    main()     
--- fuzzy_worker.py
+++ fuzzy_worker.py
@@ -1,77 +1,97 @@
+import re
 import pandas as pd
-from elasticsearch import Elasticsearch
+from elasticsearch import BadRequestError
 from elasticsearch.helpers import bulk
 
 class FuzzyWorker():
-    def __init__(self, logger, es_client, file_object):
+    def __init__(self, logger, es_client):
         self.logger = logger
         self.es_client = es_client
-        self.file_object = file_object
-        self.index_name = self.file_object.name.split(".")[0]
-        self.file_name = self.file_object.name.split(".")[0]
-        self.file_extension = self.file_object.name.split(".")[-1]
-        self.dataframe = pd.read_csv(self.file_object)
+        # self.file_object = file_object
+        # self.index_name = self.file_object.name.split(".")[0]
+        # self.file_name = self.file_object.name.split(".")[0]
+        # self.file_extension = self.file_object.name.split(".")[-1]
+        # self.dataframe = pd.read_csv(self.file_object)
 
-    def clean_dataframe(self):
+    @staticmethod
+    def clean_dataframe(df):
         # Replace "__NA__" and "N/A" with NaN first to standardize missing values
-        self.dataframe.replace(["__NA__", "N/A"], pd.NA, inplace=True)
+        df.replace(["__NA__", "N/A"], pd.NA, inplace=True)
 
         # Separate numerical and string columns
-        num_cols = self.dataframe.select_dtypes(include=['number']).columns
-        str_cols = self.dataframe.select_dtypes(include=['object', 'string']).columns
+        num_cols = df.select_dtypes(include=['number']).columns
+        str_cols = df.select_dtypes(include=['object', 'string']).columns
 
         # Fill NaNs accordingly
-        self.dataframe[num_cols] = self.dataframe[num_cols].fillna(0)
-        self.dataframe[str_cols] = self.dataframe[str_cols].fillna("No Data")
+        df[num_cols] = df[num_cols].fillna(0)
+        df[str_cols] = df[str_cols].fillna("No Data")
+        
+        # Convert all columns to string
+        df = df.astype(str)
 
-        return self.dataframe
+        return df
+    
+    @staticmethod
+    def sanitize_index_name(index_name):
+        """Replace invalid characters in Elasticsearch index name."""
+        return re.sub(r'[\\/*?"<>|, ]', '_', index_name)
 
-    def index_to_es(self):
+    def index_to_es(self, df, index_name):
         # Create index if not exists
-        if not self.es_client.indices.exists(index=self.index_name):
-            self.es_client.indices.create(index=self.index_name)
+        if not self.es_client.indices.exists(index=index_name):
+            self.es_client.indices.create(index=index_name)
 
         try:
             # Index each row
-            for i, row in self.dataframe.iterrows():
+            for i, row in df.iterrows():
                 doc = row.to_dict()
-                response = self.es_client.index(index=self.index_name, id=i, document=doc)
+                response = self.es_client.index(index=self.sanitize_index_name(index_name), id=i, document=doc)
                 self.logger.info(f"{response}")
         except Exception as e:
             self.logger.info(f"{response}")
             
 
-    def bulk_index_to_es(self):
+    def bulk_index_to_es(self, df, index_name):
+        
+        index_name = self.sanitize_index_name(index_name)
+        # Create index if not exists
+        if not self.es_client.indices.exists(index=index_name):
+            self.es_client.indices.create(index=index_name)
 
+       # try:
         # Convert DataFrame to Elasticsearch bulk format
         actions = [
             {
-                "_index": self.index_name,
+                "_index": index_name,
                 "_source": row.to_dict()
             }
-            for _, row in self.dataframe.iterrows()
+            for _, row in df.iterrows()
         ]
 
         # Bulk index documents
         success, failed = bulk(self.es_client, actions)
         self.logger.info(f"Successfully indexed {success} documents.")
-        if failed > 0:
-            self.logger.error(f"Failed to index {failed} documents.")
+            
+        #     if failed > 0:
+        #         self.logger.error(f"Failed to index {failed} documents.")
+        #         return (f"Successfully indexed {success} documents.", f"Failed to index {failed} documents.")
+        #     return f"Successfully indexed {success} documents."
+        # except BadRequestError as e:
+        #     self.logger.error(f"Elasticsearch Index Creation Failed: {e}")
+        #     return f"Elasticsearch Index Creation Failed: {e}"
+        # except Exception as e:
+        #     self.logger.error(f"Something went wrong. {e}")
+        #     return f"Something went wrong. {e}"
 
-
-    def read_and_push_to_es(self):
+    def read_and_push_to_es(self, file_object):
         try:
-            # if self.file_extension == "csv":
-            self.clean_dataframe()
-            self.bulk_index_to_es()
-                
-            # elif self.file_extension == "xlsx":
-            #     self.xlsx_sheets = pd.read_excel(self.file_object, sheet_name=None)  # Load all sheets
-            #     for sheet in self.xlsx_sheets.keys():
-            #         self.xlsx_sheets[sheet] = self.clean_dataframe(self.dataframe[sheet])
-            #         self.bulk_index_to_es(dfs[sheet], es_client, f"{file_name}_{sheet}")
+            df = pd.read_csv(file_object)
+            df = self.clean_dataframe(df)
+            self.bulk_index_to_es(df, file_object.name.split(".")[0])
+            
         except Exception as e:
             self.logger.error(f"Read and push to Elasticsearch went wrong. {e}")
+            return f"Read and push to Elasticsearch went wrong. {e}"
         
     # def get_data_from_es_index(self):
     #     # Query to fetch all documents
@@ -92,35 +112,40 @@ class FuzzyWorker():
     #     df = pd.DataFrame(data)
     #     return df
     
-    def get_data_from_es_index(self):
+    def get_data_from_es_index(self, index_name):
         """Retrieve all documents from an Elasticsearch index and return a DataFrame."""
-        scroll_time = "2m"  # Keep scroll context alive for 2 minutes
-        batch_size = 1000  # Number of docs per batch
+        if index_name:
+            # Create index if not exists
+            if self.es_client.indices.exists(index=index_name):
+                
+                scroll_time = "2m"  # Keep scroll context alive for 2 minutes
+                batch_size = 1000  # Number of docs per batch
 
-        # Initial search with scroll
-        response = self.es_client.search(
-            index=self.index_name,
-            body={"query": {"match_all": {}}},
-            scroll=scroll_time,
-            size=batch_size
-        )
+                # Initial search with scroll
+                response = self.es_client.search(
+                    index=index_name,
+                    body={"query": {"match_all": {}}},
+                    scroll=scroll_time,
+                    size=batch_size
+                )
 
-        # Extract initial batch
-        data = [doc["_source"] for doc in response["hits"]["hits"]]
-        scroll_id = response["_scroll_id"]
+                # Extract initial batch
+                data = [doc["_source"] for doc in response["hits"]["hits"]]
+                scroll_id = response["_scroll_id"]
 
-        while response["hits"]["hits"]:
-            # Fetch next batch
-            response = self.es_client.scroll(scroll_id=scroll_id, scroll=scroll_time)
-            scroll_id = response["_scroll_id"]  # Update scroll ID
-            data.extend([doc["_source"] for doc in response["hits"]["hits"]])  # Append new results
+                while response["hits"]["hits"]:
+                    # Fetch next batch
+                    response = self.es_client.scroll(scroll_id=scroll_id, scroll=scroll_time)
+                    scroll_id = response["_scroll_id"]  # Update scroll ID
+                    data.extend([doc["_source"] for doc in response["hits"]["hits"]])  # Append new results
 
-        # Clear scroll context
-        self.es_client.clear_scroll(scroll_id=scroll_id)
+                # Clear scroll context
+                self.es_client.clear_scroll(scroll_id=scroll_id)
 
-        # Convert to DataFrame
-        df = pd.DataFrame(data)
-        return df
+                # Convert to DataFrame
+                df = pd.DataFrame(data)
+                # df = self.clean_dataframe(df)
+                return df
     
     def search_elasticsearch(self, query, column):
         search_body = {
@@ -140,4 +165,62 @@ class FuzzyWorker():
         df = pd.DataFrame([hit["_source"] for hit in hits])
 
         return df
+    
+    def multi_search_elasticsearch(self, index_name, queries, fields):
+        """
+        Searches Elasticsearch with multiple queries over multiple fields.
 
+        :param queries: List of search terms or a dictionary of {field: query}
+        :param fields: List of fields to search in
+        :return: Pandas DataFrame with search results
+        """
+        # If queries is a list, create should conditions for all fields
+        if isinstance(queries, list):
+            should_conditions = [
+                {"multi_match": {"query": query, "fields": fields, "fuzziness": "2"}}
+                for query in queries
+            ]
+        # If queries is a dictionary, match each field with its respective query
+        elif isinstance(queries, dict):
+            should_conditions = [
+                {"match": {field: {"query": query, "fuzziness": "2"}}}
+                for field, query in queries.items()
+            ]
+        else:
+            raise ValueError("Queries must be a list of terms or a dictionary of {field: query}")
+
+        # Build the search body with bool query
+        search_body = {
+            "query": {
+                "bool": {
+                    "should": should_conditions,
+                    "minimum_should_match": 1  # At least one query must match
+                }
+            }
+        }
+
+        # Execute the search
+        res = self.es_client.search(index=index_name, body=search_body)
+        hits = res["hits"]["hits"]
+
+        # Convert results to DataFrame
+        df = pd.DataFrame([hit["_source"] for hit in hits])
+        
+        return df
+    
+    def get_all_indexes(self):
+        try:
+            indices = self.es_client.cat.indices(format="json")  # Fetch index metadata in JSON format
+            return [index["index"] for index in indices]  # Extract index names
+        except Exception as e:
+            print(f"Error retrieving indexes: {e}")
+            return []
+
+    def get_index_fields(self, index_name):
+        try:
+            mapping = self.es_client.indices.get_mapping(index=index_name)  # Get index mapping
+            properties = mapping[index_name]["mappings"]["properties"]  # Extract field properties
+            return list(properties.keys())  # Return field names as a list
+        except Exception as e:
+            print(f"Error retrieving fields for index '{index_name}': {e}")
+            return []
